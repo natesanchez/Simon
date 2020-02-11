5f9abf9d36572adf9ca9eef5f2c4d51db115f63e
Nate_Sanchez <NSanchez@usa.com>
2020-02-11 15:34:05 -0500

    audio test

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
-let gameTurn = []
-let playerTurn = []
+let gameOrder = [];
+let playerOrder = [];
+
+let topLeft = document.querySelector("#topLeft");
+let topRight = document.querySelector("#topRight");
+let bottomLeft = document.querySelector("#bottomLeft");
+let bottomRight = document.querySelector("#bottomRight");
+
+let greenAudio = document.querySelector("#greenAudio");
+let yellowAudio = document.querySelector("#yellowAudio")
+let blueAudio = document.querySelector("#blueAudio");
+let redAudio = document.querySelector("#redAudio")
+
+topLeft.addEventListener("click", greenSound);
+topRight.addEventListener("click", redSound);
+bottomLeft.addEventListener("click", yellowSound);
+bottomRight.addEventListener("click", blueSound);
+
+
+
+
+
+function greenSound () {
+	greenAudio.play();
+}
+
+function yellowSound () {
+	yellowAudio.play();
+}
+
+function blueSound () {
+	blueAudio.play();
+}
+
+function redSound () {
+	redAudio.play();
+}
