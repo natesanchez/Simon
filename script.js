e43b554555667f01301f9958b4b380823ccf5cff
Nate_Sanchez <NSanchez@usa.com>
2020-02-11 19:12:40 -0500

    some changes

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 let gameOrder = [];
 let playerOrder = [];
+let blink;
+let playerTurn;
+let gameTurn
+let perfect;
+let Interval;
+let sound = true
+let win;
 
+let scoreCounter = document.querySelector("h3");
+//Boxes
 let topLeft = document.querySelector("#topLeft");
 let topRight = document.querySelector("#topRight");
 let bottomLeft = document.querySelector("#bottomLeft");
 let bottomRight = document.querySelector("#bottomRight");
-
+//Audio
 let greenAudio = document.querySelector("#greenAudio");
 let yellowAudio = document.querySelector("#yellowAudio")
 let blueAudio = document.querySelector("#blueAudio");
@@ -15,8 +24,6 @@ topLeft.addEventListener("click", greenSound);
 topRight.addEventListener("click", redSound);
 bottomLeft.addEventListener("click", yellowSound);
 bottomRight.addEventListener("click", blueSound);
-
-
 
 
 
